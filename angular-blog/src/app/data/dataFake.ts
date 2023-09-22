a89fa5baed4642b0ae1668d530dabdd32feb3ab8
Dieg0Ch4ves <diegoaraujo200570@gmail.com>
2023-09-22 13:00:40 -0400

    content created

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "../../../assets/imgs/placeholder.png",
+    "title": "TITULO DE EXEMPLO PARA MOSTRAGEM",
+    "description": "Filme inedito para toda a familia assistir"
+  },
+  {
+    "id": "2",
+    "photo": "../../../assets/imgs/placeholder.png",
+    "title": "Saiu novo desenho",
+    "description": "Card menor para demonstração"
+  },
+  {
+    "id": "3",
+    "photo": "../../../assets/imgs/placeholder.png",
+    "title": "Saiu novo anime",
+    "description": "Pequeno card para mostrar"
+  },
+  {
+    "id": "4",
+    "photo": "../../../assets/imgs/placeholder.png",
+    "title": "Saiu nova serie",
+    "description": "O card está pequeno agora"
+  },
+]
